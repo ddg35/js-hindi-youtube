d078fdeb0a6c86237774f496ac6ec92860f5c6f3
ddg35 <ddg.kolkata.2017@gmail.com>
2024-05-25 13:57:00 +0000

    change done

--- 01_basics/conversion.js
+++ 01_basics/conversion.js
@@ -1,7 +1,14 @@
-let abc = "33"
+let abc = 33
 
 console.log (typeof abc)
 
-let number = Number(abc)
+let string1 = String(abc)
 
-console.log (number)
+console.log (typeof string1)
+console.log (string1)
+
+console.log(2 + "2" + "2")
+
+console.log(2 + 2 + "2")
+
+console.log("2" + "2" + 2)
