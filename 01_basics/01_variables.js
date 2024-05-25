a27f1d501f9eaab99fb368996511ae1cd0b45fd3
ddg35 <ddg.kolkata.2017@gmail.com>
2024-05-25 09:11:34 +0000

    new file

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,6 +2,10 @@ const accountId = 144553
 let accountEmail = "hitesh@google.com"
 var accountPassword = "12345"
 accountCity = "Jaipur"
+let name = "babban"
 let accountState;
 
-console.table([accountId, accountEmail, accountPassword, accountCity,accountState])
+console.table([accountId, accountEmail, accountPassword, accountCity,accountState,name])
+
+console.log(typeof name)
+console.log(typeof accountId)
